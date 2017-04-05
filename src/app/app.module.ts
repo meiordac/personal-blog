088df6293563e7572224b373ebab7023908f212e
meiordac <matias.iordache@gmail.com>
2017-04-05 19:21:21 +0200

    refactoring components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
-import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
 import { PostService } from './services/post.service';
 import { CommentService } from './services/comment.service';
 import { UserService } from './services/user.service';
 import { NewlinesPipe } from './shared/newline.pipe';
-import { CommentComponent } from './comment/comment.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { AddPostComponent } from './add-post/add-post.component';
-import { HomeComponent } from './home/home.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+import { CommentComponent } from './components/comment/comment.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
+import { HomeComponent } from './components/home/home.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { AuthGuard } from './guards/index';
 
 
@@ -29,12 +29,12 @@ import { AuthGuard } from './guards/index';
   declarations: [
     AppComponent,
     PostDetailComponent,
-    NewlinesPipe, 
-    CommentComponent, 
-    LoginComponent, 
-    RegisterComponent, 
-    AddPostComponent, 
-    HomeComponent, 
+    NewlinesPipe,
+    CommentComponent,
+    LoginComponent,
+    RegisterComponent,
+    AddPostComponent,
+    HomeComponent,
     ToolbarComponent
   ],
   imports: [
