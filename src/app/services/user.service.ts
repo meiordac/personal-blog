fff517fe9535b0ec4c6ba3599b566bde31335f51
meiordac <matias.iordache@gmail.com>
2017-05-20 18:45:01 +0200

    format user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,7 +11,7 @@ export class UserService {
 
   constructor(private http: Http) {
     // set token if saved in local storage
-    var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
     this.token = currentUser && currentUser.token;
   }
 
@@ -30,20 +30,24 @@ export class UserService {
   }
 
   login(email: string, password: string): Observable<boolean> {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-    var user = JSON.stringify({ email: email, password: password });
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const options = new RequestOptions({ headers: headers });
+    const user = JSON.stringify({ email: email, password: password });
     console.log(user);
     return this.http.post(this.apiURL + '/authenticate', user, options)
       .map((response: Response) => {
         // login successful if there's a jwt token in the response
         console.log(response.json());
-        let token = response.json() && response.json().auth_token;
+        const token = response.json() && response.json().auth_token;
         if (token) {
           // set token property
           this.token = token;
           // store username and jwt token in local storage to keep user logged in between page refreshes
-          localStorage.setItem('currentUser', JSON.stringify({ name: response.json().name, avatar: response.json().avatar, email: email, token: token }));
+          localStorage.setItem('currentUser', JSON.stringify({
+            name: response.json().name,
+            avatar: response.json().avatar,
+            email: email, token: token
+          }));
           // return true to indicate successful login
           return true;
         } else {
@@ -61,16 +65,25 @@ export class UserService {
   create(user: User) {
 
     // add authorization header with jwt token and application/json header so otherwise it wouldnt let me create comments!
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const options = new RequestOptions({ headers: headers });
 
-    var new_user = JSON.stringify({ user: { name: user.name, email: user.email, password: user.password, password_confirmation: user.password_confirmation } });
+    const new_user = JSON.stringify(
+      {
+        user:
+        {
+          name: user.name,
+          email: user.email,
+          password: user.password,
+          password_confirmation: user.password_confirmation
+        }
+      });
     return this.http.post(this.apiURL + '/users', new_user, options)
       .map((response: Response) => response.json());
   }
 
   getUser(): User {
-    var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
     return currentUser;
   }
 }
