ad29ccaf08706b71e8f8904b1866438b47576a35
meiordac <matias.iordache@gmail.com>
2017-05-21 20:05:38 +0200

    fix template for register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,34 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {UserService} from '../../services/user.service'
+import { UserService } from '../../services/user.service'
+import { User } from '../../shared/user';
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  
-  ngOnInit() {
-  }
-
-    model: any = {};
+    model: User = new User();
     error = '';
- 
+
+    ngOnInit() {
+    }
+
     constructor(
         private router: Router,
         private userService: UserService) { }
- 
+
     register() {
         this.userService.create(this.model)
             .subscribe(
-                data => {
-                    // set success message and pass true paramater to persist the message after redirecting to the login page
-                    this.router.navigate(['/login']);
-                },
-                error => {
-                    this.error = error.statusText;
-                    console.log(error);
-                });
+            data => {
+                // set success message and pass true paramater to persist the message after redirecting to the login page
+                this.router.navigate(['/login']);
+            },
+            error => {
+                this.error = error.statusText;
+                console.log(error);
+            });
     }
 }
 
