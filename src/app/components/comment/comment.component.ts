9f459b9caa75f9464ccb6377896070c882bf2d50
meiordac <matias.iordache@gmail.com>
2017-06-25 14:40:08 +0200

    add user to comment to retrieve avatar, change type of style file from css to scss

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -3,19 +3,23 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommentService } from '../../services/comment.service'
 import { Comment } from '../../shared/comment'
 import { Post } from '../../shared/post'
+import { User } from '../../shared/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.css']
+  styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
   @Input() comments: Comment[];
   @Input() post: Post;
+  user: User;
 
-  constructor(private commentService: CommentService) { }
+  constructor(private commentService: CommentService, private userService: UserService) { }
 
   ngOnInit() {
+     this.user = this.userService.getUser();
   }
 
   addComment(content: String): void {
@@ -29,11 +33,11 @@ export class CommentComponent implements OnInit {
       );
   }
 
-  upvote(comment : Comment): void {
+  upvote(comment: Comment): void {
     this.commentService.upvote(comment).subscribe();
   }
-  downvote(comment : Comment): void {
-    console.log(comment);
+  downvote(comment: Comment): void {
+    this.commentService.downvote(comment).subscribe();
   }
 
 }
