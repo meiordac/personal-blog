778111c0d99ee7754d434d5680e67ec1b155817f
meiordac <matias.iordache@gmail.com>
2017-06-25 14:42:37 +0200

    add user to post-detail component so comment component is not displayed if user is not logged in

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, Params }   from '@angular/router';
-import { Location }                 from '@angular/common';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
-import {Post} from '../../shared/post'
-import {Comment} from '../../shared/comment'
-import {PostService} from '../../services/post.service'
-import {CommentService} from '../../services/comment.service'
+import { Post } from '../../shared/post';
+import { Comment } from '../../shared/comment';
+import { PostService } from '../../services/post.service';
+import { CommentService } from '../../services/comment.service';
+import { User } from '../../shared/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-post-detail',
@@ -15,18 +17,18 @@ import {CommentService} from '../../services/comment.service'
 
 
 export class PostDetailComponent implements OnInit {
-  
+  user: User;
   post: Post;
   comments: Comment[];
- 
-  //LINKEDIN DOCS https://developer.linkedin.com/docs/share-on-linkedin#!
+
+  // LINKEDIN DOCS https://developer.linkedin.com/docs/share-on-linkedin#!
   private linkedInShare(post: Post) {
-        let shareUrl = 'http://www.linkedin.com/shareArticle?url=' + window.location;    
-        let  windowAttr='width=' + 500 + ', height=' + 400;
-        shareUrl += "&title=" + post.title;
-        shareUrl += "&summary=" + post.content.slice(0,100) + '...';
-        window.open(shareUrl, 'newwindow', windowAttr);
-      }
+    let shareUrl = 'http://www.linkedin.com/shareArticle?url=' + window.location;
+    const windowAttr = 'width=' + 500 + ', height=' + 400;
+    shareUrl += '&title=' + post.title;
+    shareUrl += '&summary=' + post.content.slice(0, 100) + '...';
+    window.open(shareUrl, 'newwindow', windowAttr);
+  }
 
   goBack(): void {
     this.location.back();
@@ -37,14 +39,17 @@ export class PostDetailComponent implements OnInit {
     private commentService: CommentService,
     private router: Router,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private userService: UserService
+    ) { }
 
-    ngOnInit() : void {  
-      this.route.params.forEach((params: Params) => {
-        let id = +params['id'];
-        this.postService.getPost(id).subscribe(post => this.post = post);
-        this.commentService.getComments(id).subscribe(comments => this.comments = comments);
+  ngOnInit(): void {
+    this.user = this.userService.getUser();
+    this.route.params.forEach((params: Params) => {
+      const id = +params['id'];
+      this.postService.getPost(id).subscribe(post => this.post = post);
+      this.commentService.getComments(id).subscribe(comments => this.comments = comments);
 
-  });
-}
+    });
+  }
 }
