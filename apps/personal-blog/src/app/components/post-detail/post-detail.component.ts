9811b5ea39ca4fe1f59a6c9177bf8998099ec030
Matias Iordache <matias.iordache@gmail.com>
2019-08-26 12:37:32 +0200

    refactor code for comment and post detail

--- apps/personal-blog/src/app/components/post-detail/post-detail.component.ts
+++ apps/personal-blog/src/app/components/post-detail/post-detail.component.ts
@@ -2,38 +2,34 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { Post } from '../../shared/post';
-import { Comment } from '../../shared/comment';
+import { Post } from '../../../../../../libs/shared/src/models/post';
+import { Comment } from '../../../../../../libs/shared/src/models/comment';
 import { PostService } from '../../services/post.service';
 import { CommentService } from '../../services/comment.service';
-import { User } from '../../shared/user';
+import { User } from '../../../../../../libs/shared/src/models/user';
 import { UserService } from '../../services/user.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.css']
 })
-
-
 export class PostDetailComponent implements OnInit {
   user: User;
   post: Post;
   comments: Comment[];
 
-  // LINKEDIN DOCS https://developer.linkedin.com/docs/share-on-linkedin#!
-  private linkedInShare(post: Post) {
-    let shareUrl = 'http://www.linkedin.com/shareArticle?url=' + window.location;
-    const windowAttr = 'width=' + 500 + ', height=' + 400;
-    shareUrl += '&title=' + post.title;
-    shareUrl += '&summary=' + post.content.slice(0, 100) + '...';
-    window.open(shareUrl, 'newwindow', windowAttr);
-  }
-
-  goBack(): void {
-    this.location.back();
-  }
-
+  /**
+   *Creates an instance of PostDetailComponent.
+   * @param {PostService} postService
+   * @param {CommentService} commentService
+   * @param {Router} router
+   * @param {ActivatedRoute} route
+   * @param {Location} location
+   * @param {UserService} userService
+   * @memberof PostDetailComponent
+   */
   constructor(
     private postService: PostService,
     private commentService: CommentService,
@@ -41,15 +37,79 @@ export class PostDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private location: Location,
     private userService: UserService
-    ) { }
+  ) {}
 
+  /**
+   *
+   *
+   * @memberof PostDetailComponent
+   */
   ngOnInit(): void {
     this.user = this.userService.getUser();
+
     this.route.params.forEach((params: Params) => {
       const id = +params['id'];
-      this.postService.getPost(id).subscribe(post => this.post = post);
-      this.commentService.getComments(id).subscribe(comments => this.comments = comments);
 
+      this.getPost(id);
+      this.fetchComments(id);
     });
+  }
+
+  // LINKEDIN DOCS https://developer.linkedin.com/docs/share-on-linkedin#!
+  /**
+   *
+   *
+   * @private
+   * @param {Post} post
+   * @memberof PostDetailComponent
+   */
+  private linkedInShare(post: Post) {
+    let shareUrl =
+      'http://www.linkedin.com/shareArticle?url=' + window.location;
+    const windowAttr = 'width=' + 500 + ', height=' + 400;
+    shareUrl += '&title=' + post.title;
+    shareUrl += '&summary=' + post.content.slice(0, 100) + '...';
+    window.open(shareUrl, 'newwindow', windowAttr);
+  }
+
+  /**
+   *
+   *
+   * @memberof PostDetailComponent
+   */
+  goBack(): void {
+    this.location.back();
+  }
+
+  /**
+   *
+   *
+   * @private
+   * @param {number} id
+   * @memberof PostDetailComponent
+   */
+  private fetchComments(id: number) {
+    this.commentService
+      .getComments(id)
+      .pipe(first())
+      .subscribe(comments => {
+        this.comments = comments;
+      });
+  }
+
+  /**
+   *
+   *
+   * @private
+   * @param {number} id
+   * @memberof PostDetailComponent
+   */
+  private getPost(id: number) {
+    this.postService
+      .getPost(id)
+      .pipe(first())
+      .subscribe(post => {
+        this.post = post;
+      });
   }
 }
